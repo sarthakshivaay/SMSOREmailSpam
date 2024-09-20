71ae997c4f3a42c4396680cc611f25e5d1455d1c
tyagisar <Sarthak.Tyagi@b-tu.de>
2024-09-20 14:19:16 +0530

    Add NLTK and update requirements

--- app.py
+++ app.py
@@ -8,11 +8,7 @@ import string
 
 nltk.download('stopwords')
 nltk.download('wordnet')
-<<<<<<< HEAD
 
-
-=======
->>>>>>> 41b324dbd23c12c12d296800bd4e41eecfdfca99
 ps=PorterStemmer()
 
 tfidf=pickle.load(open('vectorizer.pkl','rb'))
